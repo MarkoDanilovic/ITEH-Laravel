2b5990c916e3e826c88c692310b0dc6bed174212
MarkoDanilovic <markodanilovic99@gmail.com>
2022-08-29 01:43:46 +0200

    Peti -> API rute i kontroleri

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\IzdController;
+use App\Http\Controllers\PisController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('izdavac', [IzdController::class, 'index']);
+Route::get('izdavac/{izdavac}', [IzdController::class, 'show']);
+Route::put('izdavac/{izdavac}', [IzdController::class, 'update']);
+
+Route::get('pisac', [PisController::class, 'index']);
+Route::get('pisac/{pisac}', [PisController::class, 'show']);
+Route::delete('pisac/{pisac}', [PisController::class, 'destroy']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
